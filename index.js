eb7b7d7d34ec7eff626cec5edbcc3321e0e3a489
Diomandee <84577602+Diomandee@users.noreply.github.com>
2021-09-26 07:37:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+    return cats
+
+    }
+
+    function destructivelyPrependCat(name) {
+        cats.unshift('Bob')
+        return cats
+    }
+
+    function destructivelyRemoveLastCat() {
+        cats.pop()
+        return cats 
+    }
+
+    function destructivelyRemoveFirstCat() {
+        cats.shift()
+        return cats
+    }
+    function appendCat(name) {
+        let  newCats = [... cats];
+        newCats.push('Broom')
+        return newCats
+
+        
+    }
+    function prependCat(name) {
+        let  newwCats = [... cats];
+        newwCats.unshift('Arnold')
+        return newwCats
+
+        
+    }
+
+    function removeLastCat() {
+        let  newwwCats = [... cats];
+        newwwCats.pop()
+        return newwwCats
+    }
+
+    function removeFirstCat() {
+        let  newwwwCats = [... cats];
+        newwwwCats.shift()
+        return newwwwCats
+    }
